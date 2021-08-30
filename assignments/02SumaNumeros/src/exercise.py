8e73a7a9509e49ffc4224db14ca16e3aeadf3264
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-29 23:02:54 -0700

    Update exercise.py

--- assignments/02SumaNumeros/src/exercise.py
+++ assignments/02SumaNumeros/src/exercise.py
@@ -1,16 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    cont=1
-    acum=0
-    num = int(input())
-    acum+=num
-    while( acum <= 1000 ):
-        num = int(input())
-        acum+=num
-        cont+=1
-
-    print("suma =",acum)
-    print("cantidad de números =",cont)
+    pass
 
 if __name__=='__main__':
     main()
